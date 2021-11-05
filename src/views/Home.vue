0b0cf626848bcc43083e7197dd8797b53d9d0b28
DESKTOP-IU11CJP\batuh <batuhanaydonerg1998@gmail.com>
2021-11-05 17:06:54 +0300

    add: dialog to Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,23 @@
   <div>
     <search />
     <router-view></router-view>
+    <v-dialog v-model="warningDialog" width="500">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2"> Warning </v-card-title>
+
+        <v-card-text>
+          Do not send too request to API. Because, we have exceeded the rate limit per
+          minute for our plan, BASIC, by the API provider. Have fun.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="warningDialog = false"> I accept </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -10,6 +27,11 @@ import Search from "../components/Search.vue";
 export default {
   components: {
     Search,
+  },
+  data() {
+    return {
+      warningDialog: true,
+    };
   },
 };
 </script>
